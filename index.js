e563f617b8aa1d23ceb20800b87b1117c36f2dea
Yoku <yungyoku@yandex.ru>
2024-11-18 15:34:09 +0300

    Added initial List structure (partially)

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-const entry = () => console.log('Happy developing ✨')
+class ListNode {
+	value = null
+	next = null
 
-module.exports = entry
+	constructor (value) {
+		this.value = value
+	}
+}
+
+class List {
+	first = null
+	last = null
+
+	isEmpty() {
+		return this.first === null
+	}
+	addToStart(value) {
+		if (this.isEmpty()) {
+			this.first = new ListNode(value)
+		} else {
+			const newFirst = new ListNode(value)
+			newFirst.next = this.first
+
+			this.first = newFirst
+		}
+	}
+
+	get(index) {
+		let i = 0
+		for (const item of this) {
+			if (i === index) return item
+			i++
+		}
+	}
+
+	*[Symbol.iterator]() {
+		let listNode = this.first
+
+		while (listNode !== null) {
+			const value = listNode.value
+
+			listNode = listNode.next
+
+			yield value
+		}
+	}
+}
+
+module.exports = { List }
