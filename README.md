0c5b48bccbd1185ad4648f8ae756b14c1bcee832
Yoku <yungyoku@yandex.ru>
2024-11-20 18:23:07 +0300

    Refactoring

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Data structures project
+# Data structures [![npm](https://img.shields.io/npm/v/simple-structures.svg)](https://www.npmjs.com/package/simple-structures)
 
 Current structures: List
---
+++ list.js
@@ -0,0 +1,102 @@
+class ListNode {
+	value = null
+	next = null
+
+	constructor (value) {
+		this.value = value
+	}
+}
+
+class List {
+	#first = null
+	#last = null
+
+	isEmpty() {
+		return this.#first === null
+	}
+
+	addToStart(value) {
+		const node = new ListNode(value)
+
+		if (this.isEmpty()) {
+			this.#first = node
+		} else {
+			node.next = this.#first
+
+			if (this.#last === null) {
+				this.#last = this.#first
+			}
+
+			this.#first = node
+		}
+	}
+	addToEnd(value) {
+		const node = new ListNode(value)
+
+		if (this.isEmpty()) {
+			this.#first = node
+		} else {
+			if (this.#last === null) {
+				this.#first.next = node
+				this.#last = node
+			} else {
+				this.#last.next = node
+
+				this.#last = node
+			}
+		}
+	}
+
+	get(index) {
+		if (index < 0) throw new Error('Index out of bounds')
+
+		let i = 0
+		for (const item of this) {
+			if (i === index) return item
+			i++
+		}
+	}
+	getFirst() {
+		return this.#first
+	}
+	getLast() {
+		return this.#last
+	}
+
+	remove(index) {
+		if (index < 0) throw new Error('Index out of bounds')
+
+		let i = 0
+		let listNode = this.#first
+
+		while (i !== index - 1) {
+			i++
+
+			if (listNode.next) {
+				listNode = listNode.next
+			} else {
+				throw new Error('Index out of bounds')
+			}
+		}
+
+		listNode.next = listNode.next.next
+	}
+	removeAll() {
+		this.#first = null
+		this.#last = null
+	}
+
+	*[Symbol.iterator]() {
+		let listNode = this.#first
+
+		while (listNode !== null) {
+			const value = listNode.value
+
+			listNode = listNode.next
+
+			yield value
+		}
+	}
+}
+
+export default List
